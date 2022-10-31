090191f90db78c4c7b1eea7a2d5703297926befd
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2022-10-31 15:44:23 +0300

    feat: add card menu

--- netflixroulette/src/components/movieCard.tsx
+++ netflixroulette/src/components/movieCard.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import styles from '../styles/MovieCard.module.css';
+import burger from '../assets/images/burger.svg';
+import cross from '../assets/images/cross.svg';
 
 export interface IMovieCardProps {
   id: number;
@@ -9,9 +11,43 @@ export interface IMovieCardProps {
   image: string;
 }
 
-export function MovieCard({ id, title, year, genres, image }: IMovieCardProps):JSX.Element {
+export function MovieCard({
+  id,
+  title,
+  year,
+  genres,
+  image,
+}: IMovieCardProps): JSX.Element {
+  const [burgerMenuIsOpen, setIsBurgerMenuIsOpen] = React.useState(false);
+
+  const handleClickBurgerOpen = () => {
+    setIsBurgerMenuIsOpen(true);
+  };
+
+  const handleClickBurgerClose = () => {
+    setIsBurgerMenuIsOpen(false);
+  };
+
   return (
     <div className={styles.conteiner}>
+      <img
+        className={styles.burger}
+        src={burger}
+        alt='burger icon'
+        onClick={handleClickBurgerOpen}
+      />
+      {burgerMenuIsOpen ? (
+        <div className={styles.burgerMenu}>
+          <img
+            className={styles.burger}
+            src={cross}
+            alt='cross icon'
+            onClick={handleClickBurgerClose}
+          />
+          <p>Edit</p>
+          <p>Delete</p>
+        </div>
+      ) : null}
       <img src={image} alt='movie poster' />
       <div className={styles.titleConteiner}>
         <p className={styles.title}>{title}</p>
